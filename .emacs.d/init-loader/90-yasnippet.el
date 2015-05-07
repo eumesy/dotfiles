93e622ab46c431e738cb1382626e022d0888849f
Sho Yokoi <sho.yokoi@gmail.com>
2015-05-07 16:02:48 +0900

    yasnippet 起動のキーバインド変更

--- .emacs.d/init-loader/90-yasnippet.el
+++ .emacs.d/init-loader/90-yasnippet.el
@@ -3,7 +3,17 @@
       '("~/.emacs.d/snippets"
         ;; "~/.emacs.d/elpa/yasnippet-20131021.928/snippets"
         ))
+
 (yas-global-mode 1)
+
+;; keybind
+;; ref. http://stackoverflow.com/questions/14066526/unset-tab-binding-for-yasnippet
+;; remove
+(define-key yas-minor-mode-map [(tab)]       nil)
+(define-key yas-minor-mode-map (kbd "TAB")   nil)
+(define-key yas-minor-mode-map (kbd "<tab>") nil)
+;; define
+(define-key yas-minor-mode-map (kbd "C-M-i") 'yas-expand)
 
 (require 'helm-c-yasnippet)
 (global-set-key (kbd "C-M-;") 'helm-c-yas-complete)
