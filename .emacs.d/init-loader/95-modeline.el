b3827726bcb0501088a62ffbd95295f7eca06132
Sho Yokoi <sho.yokoi@gmail.com>
2015-05-14 18:54:27 +0900

    mode-line 日付も表示

--- .emacs.d/init-loader/95-modeline.el
+++ .emacs.d/init-loader/95-modeline.el
@@ -1,6 +1,57 @@
-;; display column number on mode line
-(column-number-mode t)
+;; default を触るべきではなかった感
+;; 触りはじめてしまった以上仕方ない
+(setq-default mode-line-format
+              '(
+                "("
+                (line-number-mode "%l:")
+                (column-number-mode "%c")
+                ")"
+                mode-line-mule-info
+                mode-line-modified
+                mode-line-frame-identification
+                mode-line-buffer-identification
+                " "
+                global-mode-string
+                " "
+                mode-name
+                mode-line-process
+                minor-mode-alist
+                "%n"
+                " "
+                (which-func-mode ("" which-func-format "--"))
+                (-3 . "%p")
+                "-%-"
+                ))
+(setq column-number-mode t)
+(setq line-number-mode t)
 
-;; 時刻表示
-(display-time-mode 1)
+;; smart-mode-line
+;; https://github.com/Malabarba/smart-mode-line/
+(require 'smart-mode-line)
+;;; これがないと表示がはみでる
+(setq sml/extra-filler -10)
+;;; ファイル名 max-width
+(setq sml/name-width 38)
+;;; everything after the minor-modes will be right-indented
+(setq sml/mode-width 'full)
+;;; 表示しない mode. 手前に空白スペース要?
+(setq sml/hidden-modes
+      '(
+        " Helm"
+        ))
+;;; 'Loading a theme can run Lisp code.  Really load? (y or n)' と確認を取らない
+(setq sml/no-confirm-load-theme t)
+(sml/setup)
+(setq sml/theme 'respectfull)
 
+(autoload 'display-time "time" "Display time on Mode-line" t)
+;; -> global-mode-string
+;;; format:
+;;; - month day dayname
+;;; - 24-hours minutes
+(setq display-time-string-forms
+      '((format "%s/%s(%s)%s:%s"
+                month day dayname
+                24-hours minutes
+                )))
+(display-time)
