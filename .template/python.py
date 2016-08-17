fbee6ab17211daa89b372a28176a1fa1a32bb62f
Sho Yokoi <sho.yokoi@gmail.com>
2016-08-17 22:19:46 +0900

    upload python template

---
+++ .template/python.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8; mode: Python -*-
+
+import argparse
+
+def main():
+
+
+if __name__ == '__main__':
+    # parser
+    parser = argparse.ArgumentParser(description='...')
+    args = parser.parse_args()
+
+    main()
