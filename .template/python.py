d4b2cf59dbe3264b14809c977e1bca0664f8ef3d
Sho Yokoi <sho.yokoi@gmail.com>
2016-10-24 18:09:06 +0900

    modify template: python

--- .template/python.py
+++ .template/python.py
@@ -10,8 +10,27 @@ if __name__ == '__main__':
     # parser
     parser = argparse.ArgumentParser(description='...')
 
+
     # positional arguments
-    parser.add_argument('path_to_src')
+    parser.add_argument('arg_name', type=int,
+                        help='...')
+
+    # optional arguments
+    parser.add_argument('-f', '-foo', type=int, default=0, # default の default: None
+                        help='...')
+
+    # optional arguments (flags)
+    parser.add_argument('-f', 'foo', action='store_true',
+                        help='...')
+
+    # choice
+    #   type=int, choice=[1,2,4]
+    # opened file
+    #   type=open
+    #   type=argparse.FileType('w', encoding='UTF-8')
+    # required (for optional arguments) (generally considered bad form)
+    #   required=True
+
 
     # parse
     args = parser.parse_args()
